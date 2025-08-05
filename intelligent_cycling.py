0a3dce0884bffcafe8879d634315a70a11ff23d8
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-08-05 20:03:40 +0200

    Make login functionality a function

--- intelligent_cycling.py
+++ intelligent_cycling.py
@@ -1,53 +1,28 @@
-# Import required libraries
-import logging
-import os
-import time
-from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-
-# Load environment variables
-load_dotenv()
-
-# Retrieve credentials from environment variables
-IC_USER = os.getenv("IC_USER")
-IC_PASS = os.getenv("IC_PASS")
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Configure Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--headless")  # Run in headless mode, no GUI
-
-# Initialise the Chrome web driver
-driver = webdriver.Chrome(options=chrome_options)
-
 def intelligent_cycling_login():
+    # Import required libraries
+    import os
+    import logging
+    from dotenv import load_dotenv
     from selenium import webdriver
     from selenium.webdriver.common.by import By
     from selenium.webdriver.chrome.options import Options
-    import logging
-    import time
-    import os
-    from dotenv import load_dotenv
 
+    # Set up configuration
     load_dotenv()
     IC_USER = os.getenv("IC_USER")
     IC_PASS = os.getenv("IC_PASS")
 
+    # Configure logging
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
+    # Configure Chrome options
     chrome_options = Options()
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--headless")
 
+    # Initialise the Chrome web driver
     driver = webdriver.Chrome(options=chrome_options)
     try:
         logger.info("Navigating to the login page")
