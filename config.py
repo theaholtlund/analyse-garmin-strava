6170a0c1e6e53ed63a3d101a44aa6560e00d41b5
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-08-07 08:58:01 +0200

    Add initial config file for shared functionality

---
+++ config.py
@@ -0,0 +1,23 @@
+# Import required libraries
+import os
+import logging
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Load the credentials from Garmin
+GARMIN_USER = os.getenv("GARMIN_USER")
+GARMIN_PASS = os.getenv("GARMIN_PASS")
+
+# Load the credentials from Strava
+STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
+STRAVA_REDIRECT_URI = os.getenv("STRAVA_REDIRECT_URI", "http://localhost")
+
+# Load the credentials from Intelligent Cycling
+IC_USER = os.getenv("IC_USER")
+IC_PASS = os.getenv("IC_PASS")
+
+# Load the credentials from Todoist
+TODOIST_API_TOKEN = os.getenv("TODOIST_API_TOKEN")
