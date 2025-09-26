3e281e6a45222ffa676215fa1cd9456354a77dcb
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-09-26 14:27:06 +0200

    Initial dashboard file

---
+++ dashboard.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from config import logger
+from garmin_connect import prepare_dataframe
+
+def filter_running_activities(df):
+    if df.empty:
+        logger.info("Received empty dataframe in filter_running_activities")
+        return pd.DataFrame()
+
+    df = prepare_dataframe(df)
+
+    running_keys = [
+        "running", "indoor_running", "treadmill_running", "track_running",
+        "street_running", "obstacle_run", "ultra_run", "trail_running",
+        "virtual_run", "multisport"
+    ]
+
+    df_running = df[df['activityTypeKey'].isin(running_keys)].copy()
+    df_running['distance_km'] = df_running['distance'] / 1000 
+
+    df_running['pace_min_per_km'] = df_running.apply(
+        lambda r: (r['duration'] / 60 / r['distance_km']) if r['distance_km'] > 0 else None, axis=1
+    )
