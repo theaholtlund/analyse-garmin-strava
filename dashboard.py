5ed858554e2563fdb8750f13a00aee50ee6ee647
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-09-27 11:31:40 +0200

    Add intial functionality to display plot

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,11 @@
 # Import required libraries
+import datetime
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Import shared configuration and functions from other scripts
-from config import logger
-from garmin_connect import prepare_dataframe
+from config import logger, check_garmin_credentials, RUNNING_THROUGH_GITHUB
+from garmin_connect import fetch_data, prepare_dataframe
 from utils import ensure_dir
 
 # Ensure outputs directory exists
@@ -29,3 +31,32 @@ def filter_running_activities(df):
 
     logger.info("Filtered %d running activities (including multisport legs) out of %d total", len(df_running), len(df))
     return df_running
+
+
+def generate_dashboard():
+    garmin_creds = check_garmin_credentials()
+
+    today = datetime.date.today()
+    start_of_year = datetime.date(today.year, 1, 1)
+    logger.info("Fetching activities from %s to %s", start_of_year, today)
+
+    _, df_all = fetch_data(start_of_year, today, garmin_creds)
+    if df_all.empty:
+        return
+
+    df_running = filter_running_activities(df_all)
+    if df_running.empty:
+        return
+
+    fig = plt.figure(constrained_layout=True, figsize=(14, 10))
+
+    dashboard_path = PLOTS_DIR + "/run_distance.png"
+    fig.savefig(dashboard_path, dpi=150)
+    plt.show()
+
+
+if __name__ == "__main__":
+    if RUNNING_THROUGH_GITHUB:
+        logger.warning("Dashboard not generated when running in GitHub Actions")
+    else:
+        generate_dashboard()
