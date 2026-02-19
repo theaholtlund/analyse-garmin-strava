b509349ee1335587f15241fe353ec6ed27a07ed9
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2026-02-19 06:24:47 +0100

    Set function docstring

--- weekly_report.py
+++ weekly_report.py
@@ -4,6 +4,7 @@ from email.message import EmailMessage
 from config import load_env
 
 def send_email(subject, body):
+    """Send plain-text e-mail using SMTP credentials from environment variables."""
     msg = EmailMessage()
     msg["From"] = load_env("REPORT_EMAIL_FROM")
     msg["To"] = load_env("REPORT_EMAIL_TO")
